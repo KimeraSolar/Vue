1311a440b57189712c5c7e1a01ade0af5275e0f8
LBBassani <lorena.b.bassani@gmail.com>
2024-08-01 16:22:05 -0300

    :sparkles: load products from api

--- Project 5 - Shopping Bag Initial/src/store/index.js
+++ Project 5 - Shopping Bag Initial/src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
-    
+    products: [],
   },
   mutations: {
-    
+    setProducts(state, products){
+      state.products = products;
+    }
   },
   actions: {
-
+    async loadProducts({commit}){
+      const resp = await axios.get('https://fakestoreapi.com/products');
+      commit('setProducts', resp.data);
+    }
   },
   modules: {
   }
