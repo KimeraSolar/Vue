e793bae44a61c22cfe1ce921d3f4866f881c7b54
LBBassani <lorena.b.bassani@gmail.com>
2024-08-06 17:20:09 -0300

    :tada: begin studies of charts libs in Vue

---
+++ vue-charts/src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<template>
+  <header>
+    <span class="container">
+      <h1 class="green">Charts</h1>
+      <p>Estudo de libs para montar charts no Vue</p>
+    </span>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
