e02f201ba926ecc5aa4bef2d060a2aea447805d7
LBBassani <lorena.b.bassani@gmail.com>
2024-08-06 17:27:43 -0300

    :construction: apexchart initialization

--- vue-charts/src/App.vue
+++ vue-charts/src/App.vue
@@ -1,4 +1,26 @@
 <script lang="ts">
+export default {
+  name:'App',
+  data(){
+    return{
+      options:{
+        chart:{
+          id: 'vuechart-example'
+        },
+        xaxis:{
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [
+        {
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    }
+  }
+
+}
 </script>
 
 <template>
@@ -10,6 +32,7 @@
   </header>
 
   <main>
+    <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
   </main>
 </template>
 
